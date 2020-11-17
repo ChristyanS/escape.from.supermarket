36469432cefaf40f71a682014db1256b1fe80a77
Christyan Santos <christyanrosa@hotmail.com>
2020-11-16 21:45:21 -0300

    adding basic movement

---
+++ Assets/Scripts/Behaviours/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _characterController;
+    public float moveSpeed;
+    public float rotationSpeed;
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+        var direction =  transform.TransformDirection(new Vector3(0,0,Input.GetAxis("Vertical")));
+        _characterController.SimpleMove(direction * moveSpeed);
+    }
+}
