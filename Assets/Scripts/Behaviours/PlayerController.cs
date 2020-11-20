c2ecdbdf802f902de6728f1b5196ccfddd6abf1a
Christyan Santos <christyanrosa@hotmail.com>
2020-11-19 23:18:35 -0300

    first version of movement

--- Assets/Scripts/Behaviours/PlayerController.cs
+++ Assets/Scripts/Behaviours/PlayerController.cs
@@ -1,23 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Cinemachine;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
+using UnityEngine.Animations;
 
-public class PlayerController : MonoBehaviour
+namespace Behaviours
 {
-    private CharacterController _characterController;
-    public float moveSpeed;
-    public float rotationSpeed;
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        _characterController = GetComponent<CharacterController>();
-    }
+        private CharacterController _characterController;
+        private Vector3 _direction;
+        private float _acceleration;
+        private Vector3 _cameraRight;
+        private Vector3 _cameraForward;
+        private Vector3 _movePlayer;
+        public Transform cameraTransform;
+        public float movementSpeed = 2.0f;
+        public float gravity = 9.8f;
+        public float fallVelocity;
+        public float jumpForce;
 
-    void FixedUpdate()
-    {
-        transform.Rotate(new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0));
-        var direction =  transform.TransformDirection(new Vector3(0,0,Input.GetAxis("Vertical")));
-        _characterController.SimpleMove(direction * moveSpeed);
+        void Start()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        void FixedUpdate()
+        {
+            var horizontalAxis = Input.GetAxis("Horizontal");
+            var verticalAxis = Input.GetAxis("Vertical");
+            _direction = new Vector3(horizontalAxis, 0, verticalAxis);
+
+            CameraDirection();
+            _movePlayer = _direction.x * _cameraRight + _direction.z * _cameraForward;
+
+            _characterController.transform.LookAt(_characterController.transform.position + _movePlayer);
+
+            SetGravity();
+            _characterController.Move(_movePlayer * (movementSpeed * Time.deltaTime));
+        }
+
+        void SetGravity()
+        {
+            if (_characterController.isGrounded)
+                fallVelocity = -gravity * Time.deltaTime;
+            else
+                fallVelocity -= gravity * Time.deltaTime;
+            _movePlayer.y = fallVelocity;
+        }
+
+        void Jump()
+        {
+            if (_characterController.isGrounded && Input.GetButtonDown("Jump"))
+            {
+                fallVelocity = jumpForce;
+            }
+        }
+
+        void CameraDirection()
+        {
+            _cameraForward = cameraTransform.forward;
+            _cameraRight = cameraTransform.right;
+
+            _cameraForward.y = 0;
+            _cameraRight.y = 0;
+
+            _cameraForward = _cameraForward.normalized;
+            _cameraRight = _cameraRight.normalized;
+        }
     }
 }
