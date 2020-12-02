e335d1243f3d6312e4133f03dd9a0b6416129bd1
Christyan Santos <christyanrosa@hotmail.com>
2020-12-01 22:07:03 -0300

    adding menu scene

---
+++ Assets/Scripts/Behaviours/UI/MenuView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Behaviours.UI
+{
+    public class MenuView : MonoBehaviour
+    {
+
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
