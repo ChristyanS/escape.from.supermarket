cb46535856069a154ba84c5788ca87d5e15b85a3
Christyan Santos Rosa <christyanrosa@hotmail.com>
2020-12-18 12:03:43 -0300

    adding basic button trigger

---
+++ Assets/Scripts/Behaviours/Actions/ButtonAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Behaviours.Actions
+{
+    public class ButtonAction : MonoBehaviour
+    {
+        public GameObject targetObject;
+        private bool _trigged;
+        private static readonly int Trigger = UnityEngine.Animator.StringToHash("trigger");
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!_trigged)
+            {
+                GetComponentInParent<UnityEngine.Animator>().SetBool(Trigger,true);
+                targetObject.GetComponent<UnityEngine.Animator>().SetBool(Trigger, true);
+                _trigged = true;
+            }
+        }
+    }
+}
