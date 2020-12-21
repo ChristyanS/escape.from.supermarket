0bfc92267cc05c9a0f6f0137c165281e33b68f80
Christyan Santos Rosa <christyanrosa@hotmail.com>
2020-12-20 23:18:51 -0300

    adding basic load scene

--- Assets/Scripts/Behaviours/UI/MenuView.cs
+++ Assets/Scripts/Behaviours/UI/MenuView.cs
@@ -1,16 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Behaviours.UI
 {
     public class MenuView : MonoBehaviour
     {
+        public GameObject loadPanel;
 
         private void Update()
         {
             if (Input.GetKey(KeyCode.Return))
             {
-                SceneManager.LoadScene(1);
+                loadPanel.SetActive(true);
+                StartCoroutine(LoadScene());
+            }
+        }
+        
+        private IEnumerator LoadScene()
+        {
+            var scene = SceneManager.LoadSceneAsync(1);
+            while (!scene.isDone)
+            {
+                yield return null;
+                Time.timeScale = 1f;
             }
         }
     }
