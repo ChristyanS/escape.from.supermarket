be3e3ce11c99bd2e02baeaec8f28986a4448697b
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-02-02 20:54:57 -0300

    adding sound manager

---
+++ Assets/Scripts/Behaviours/Managers/SoundsManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Behaviours.Managers
+{
+    public class SoundsManager : Singleton<SoundsManager>
+    {
+        private AudioSource[] _audioSources;
+        [SerializeField] [Range(0, 1)] private float volume = 1;
+
+        private void Start()
+        {
+            _audioSources = FindObjectsOfType<AudioSource>();
+            foreach (var audioSource in _audioSources)
+            {
+                audioSource.volume = volume;
+            }
+        }
+    }
+}
