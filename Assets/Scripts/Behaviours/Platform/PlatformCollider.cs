1b5754adfc092bcafd37bf2387f5f7e5c9eb6891
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-04-24 18:38:26 -0300

    fixing float on the lift

--- Assets/Scripts/Behaviours/Platform/PlatformCollider.cs
+++ Assets/Scripts/Behaviours/Platform/PlatformCollider.cs
@@ -8,32 +8,6 @@ public class PlatformCollider : MonoBehaviour
     public GameObject Player;
 
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    Debug.Log("Collided with something");
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-
-    //                Player.transform.parent = transform;
-    //        Debug.Log("Collided with player");
-    //    }
-        
-    //}
-
-    //private void OnCollisionExit(Collision collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-
-    //        Player.transform.parent = null;
-
-    //    }
-    //}
-
-
-
-
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
