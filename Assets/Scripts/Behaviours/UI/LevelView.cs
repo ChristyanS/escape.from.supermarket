c9f63cccadb35e2d6575b5d871492d95168634af
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-04-25 00:06:19 -0300

    adding winner message text

--- Assets/Scripts/Behaviours/UI/LevelView.cs
+++ Assets/Scripts/Behaviours/UI/LevelView.cs
@@ -21,18 +21,7 @@ namespace Behaviours.UI
                 SceneManager.LoadScene(scene);
                 VirtualInputManager.Instance.EnableAllControls(true);
             }
-
-            if (Input.GetKey(KeyCode.Alpha1))
-            {
-                SceneManager.LoadScene(1);
-                VirtualInputManager.Instance.EnableAllControls(true);
-            }
-
-            if (Input.GetKey(KeyCode.Alpha2))
-            {
-                SceneManager.LoadScene(2);
-                VirtualInputManager.Instance.EnableAllControls(true);
-            }
+            
         }
     }
 }
